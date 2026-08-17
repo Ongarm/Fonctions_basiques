08fdf0aadedddbbf2a76dff744ad98894ff9f254
mbougear <mbougear@f2r11s7.paris.42.school>
2026-08-17 18:31:07 +0200

    Inclusion de putchar dans putnbr et simplification de l'entier minimal.

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,32 +6,22 @@
 /*   By: mbougear <mbougear@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 16:23:44 by mbougear          #+#    #+#             */
-/*   Updated: 2026/08/17 16:39:36 by mbougear         ###   ########.fr       */
+/*   Updated: 2026/08/17 18:29:56 by mbougear         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+#include <unistd.h>
 
-void	ft_put_int_min(void)
+void	ft_putchar(char c)
 {
-	ft_putchar('-');
-	ft_putchar('2');
-	ft_putchar('1');
-	ft_putchar('4');
-	ft_putchar('7');
-	ft_putchar('4');
-	ft_putchar('8');
-	ft_putchar('3');
-	ft_putchar('6');
-	ft_putchar('4');
-	ft_putchar('8');
+	write(1, &c, 1};
 }
 
 void	ft_putnbr(int n)
 {
 	if (n == -2147483648)
 	{
-		ft_put_int_min();
+		write(1, "-2147483648", 11);
 		return ;
 	}
 	if (n < 0)
